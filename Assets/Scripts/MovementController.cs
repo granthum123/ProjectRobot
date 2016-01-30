f4ef4e07305544fc5623e22a301cface876fbb4f
Do Won Cha <dowoncha@live.unc.edu>
2016-01-29 21:17:51 -0500

    Movement and Uesr controllers

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+    public class MovementController : MonoBehaviour
+    {
+        public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+        public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+
+        private void Awake()
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        public void Move(float inputValue)
+        {
+            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+            Vector3 movement = transform.forward * inputValue * m_Speed * Time.deltaTime;
+
+            // Apply this movement to the rigidbody's position.
+            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        }
+
+        public void Turn(float inputValue)
+        {
+            // Determine the number of degrees to be turned based on the input, speed and time between frames.
+            float turn = inputValue * m_TurnSpeed * Time.deltaTime;
+
+            // Make this into a rotation in the y axis.
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+            // Apply this rotation to the rigidbody's rotation.
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        }
+    }
---
+++ Assets/Scripts/UserController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserController : MonoBehaviour {
+
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;
+    private Movement m_movement;
+
+    private void Start()
+    {
+        m_movement = GetComponent<Movement>();
+        // The axes names are based on player number.
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+    }
+
+
+    private void Update()
+    {
+        // Store the value of both input axes.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+    }
+
+    private void FixedUpdate()
+    {
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        m_movement.Move(m_MovementInputValue);
+        m_movement.Turn(m_TurnInputValue);
+    }
+}
