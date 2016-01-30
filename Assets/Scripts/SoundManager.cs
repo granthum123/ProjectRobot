7f520ad03841a6a0a56ada00ba43fd365b5647e0
Ian Henry <ianhenry26@gmail.com>
2016-01-30 15:39:15 -0500

    added audio functionality

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+    #region Variables
+    ///Singleton
+    private static SoundManager instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<SoundManager>();
+
+            return instance;
+        }
+    }
+    #endregion
+    public AudioSource Play(AudioClip clip, Transform emitter)
+    {
+        return Play(clip, emitter, 1f, 1f);
+    }
+
+    public AudioSource Play(AudioClip clip, Transform emitter, float volume)
+    {
+        return Play(clip, emitter, volume, 1f);
+    }
+
+    public AudioSource Play(AudioClip clip, Transform emitter, float volume, float pitch)
+    {
+        //Create an empty game object
+        GameObject go = new GameObject("Audio: " + clip.name);
+        go.transform.position = emitter.position;
+        go.transform.parent = emitter;
+
+        //Create the source
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+        Destroy(go, clip.length);
+        return source;
+    }
+
+    public AudioSource Play(AudioClip clip, Vector3 point)
+    {
+        return Play(clip, point, 1f, 1f);
+    }
+
+    public AudioSource Play(AudioClip clip, Vector3 point, float volume)
+    {
+        return Play(clip, point, volume, 1f);
+    }
+    public AudioSource Play(AudioClip clip, Vector3 point, float volume, float pitch)
+    {
+        //Create an empty game object
+        GameObject go = new GameObject("Audio: " + clip.name);
+        go.transform.position = point;
+
+        //Create the source
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+        Destroy(go, clip.length);
+        return source;
+    }
+    public AudioSource PlayLoop(AudioClip clip, Transform emitter)
+    {
+        //Create an empty game object
+        GameObject go = new GameObject("Audio: " + clip.name);
+        go.transform.position = emitter.position;
+        go.transform.parent = emitter;
+
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = true;
+        source.Play();
+        //Destroy(go, clip.length);
+        return source;
+    }
+}
