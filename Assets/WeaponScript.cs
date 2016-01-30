bbe7a017e126c61cd58ed696919c75e8d2d9fd66
Do Won Cha <dowoncha@live.unc.edu>
2016-01-29 20:50:15 -0500

    managers

--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -3,28 +3,47 @@ using System.Collections;
 
 public class WeaponScript : MonoBehaviour, WeaponInterface {
 
-	public GameObject bullet;
+	public int PlayerNumber = 1;
+	public Rigidbody m_Shell;
+	public Transform m_FireTransform;
+	public float m_MinLaunchForce = 15.0f;
+	public float m_MaxLaunchForce = 30.0f;
+	public float m_MaxChargeTime = .75f;
+
+	private float m_CurrentLaunchForce;
+	private float m_ChargeSpeed;
+	private bool m_Fired = false;
+
+	private void OnEnable()
+	{
+		m_CurrentLaunchForce = m_MinLaunchForce;
+	}
 
 	// Use this for initialization
 	public void Start () {
+
+		// Charge speed is delta force per charge time
+		m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
 	}
 	
 	// Update is called once per frame
-	public void Update () {
-		if(Input.GetKeyDown(KeyCode.F))
-		{
-			Fire();
-		}
-		if (Input.GetKeyDown (KeyCode.D)) {
-			Destroy (bullet.gameObject, 0.5f);
+	public void Update (){
+
+		// If current force is greater than launch and still haven't fired.
+		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
+			m_CurrentLaunchForce = m_MaxLaunchForce;
+			Fire ();
 		}
 	}
 
 	public void Fire()
 	{
-		Instantiate(bullet);
-		Transform currentLoc = GetComponent<Transform>();
-		bullet.transform.position = currentLoc.position;
+		m_Fired = true;
+
+		Rigidbody shellInstance = Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation ) as Rigidbody;
+		shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+		m_CurrentLaunchForce = m_MinLaunchForce;
 	}
 
 	public void Drop()
