00023ef5db99cb0efcb44479e187f73f1959ae1f
Do Won Cha <dowoncha@live.unc.edu>
2016-01-29 20:45:02 -0500

    added robot and game manager

---
+++ Assets/RobotManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotManager : MonoBehaviour {
+
+	public Color m_PlayerColor;				
+	public Transform m_SpawnPoint;
+	[HideInInspector] public int m_PlayerNumber;
+	[HideInInspector] public GameObject m_Instance;
+
+	private Movement m_Movement;
+	private WeaponInterface m_LeftWeapon;
+	private WeaponInterface m_RightWeapon;
+	//private AbilityInterface m_Ability;
+
+	public void Setup()
+	{
+
+	}
+}
